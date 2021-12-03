2a2378c67790e28fc6f06bed0f346b7757a7bd46
Muqtadir Ahmed <muqtadir.ahmed@kochtc.com>
2021-12-04 01:20:46 +0530

    Trying something out

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Table } from "antd";
+import { useState, useEffect } from "react";
+
+const columns = [
+  {
+    key: "questions",
+    title: "Questions",
+    dataIndex: ["qData", "En", "text"],
+    render: (value) => {
+      return (
+        <div
+          key={value}
+          dangerouslySetInnerHTML={{
+            __html: value,
+          }}
+        ></div> // Another way of doing it: https://github.com/wrakky/react-html-parser
+      );
+    },
+  },
+];
 
 function App() {
+  const [state, setState] = useState({
+    data: [],
+    loading: true,
+  });
+
+  async function getQuestions() {
+    try {
+      const response = await fetch("<KEY_IN_YOUR_URL_HERE>");
+      const data = await response.json();
+      setState({ data: data.data, loading: false });
+    } catch (error) {
+      console.log("ERROR: ", error);
+      setState((prevValue) => Object.assign({}, prevValue, { loading: false }));
+    }
+  }
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Table
+      rowKey={(value) => value._id}
+      loading={state.loading}
+      dataSource={state.data.data ?? []}
+      columns={columns}
+    />
   );
 }
 
